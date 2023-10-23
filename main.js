d8d10b3844890489791a243e27189495da797a2b
guizi17 <127101601+guizi17@users.noreply.github.com>
2023-10-23 08:07:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-// Pom --------------------------------------
-function tocaSomPom () {
+function tocaSom () {
     document.querySelector('#som_tecla_pom'). play();
 }
 
 const listaDeTeclas = Document.querySelectorll ('.tecla');
 
+let contador = 0;querySelectorquerySelector
 
 //enquanto
-white(){
-    listaDeTeclas[0].onclik = tocaSomPom
-}
+    white(contador <  listaDeTeclas.length) {
+    
+        listaDeTeclas[0].onclik = tocaSomPom;
+    
+        listaDeTeclas[contador].onclik =  tocaSomPom;
+
+        contador = contador + tocaSomPom;
+
+        console.log(contador);
+
+    }
