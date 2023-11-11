39755d76e7c108cbfbc3f6e4df4a6e4af7cc891d
guizi17 <127101601+guizi17@users.noreply.github.com>
2023-11-10 21:46:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,96 +1,86 @@
-const keySongs = [
-    {
-        key: 1,
-        song: 'pom',
-        down: false
-    },
-    {
-        key: 2,
-        song: 'clap',
-        down: false
-    },
-    {
-        key: 3,
-        song: 'tim',
-        down: false
-    },
-    {
-        key: 4,
-        song: '41anoos',
-        down: false
-    },
-    {
-        key: 5,
-        song: 'splash',
-        down: false
-    },
-    {
-        key: 6,
-        song: 'toim',
-        down: false
-    },
-    {
-        key: 7,
-        song: 'psh',
-        down: false
-    },
-    {
-        key: 8,
-        song: 'tic',
-        down: false
-    },
-    {
-        key: 9,
-        song: 'tom',
-        down: false
-    }
-]
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-function tocaSom(audioElement) {
-    audioElement.currentTime = 0
-    audioElement.play()
-}
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-keySongs.forEach(song => {
-    let keyElement = document.querySelector(`.tecla_${song.song}`)
+    <title>Internacional audios</title>
 
-    song.keyElement = keyElement
-    song.audioElement = document.getElementById(`som_tecla_${song.song}`)
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&display=swap" rel="stylesheet">
 
-    keyElement.addEventListener('click', () => {
-        tocaSom(song.audioElement)
-    })  
+      <link rel="icon" type="image/png" href="imagem/logointer.png">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/estilos.css">
 
-})
+</head>
 
-document.onkeydown = (event) => {
-    const key = event.key
-    const keySong =  keySongs.find(keySong => keySong.key == key)
-   
-    if(keySong) {
+<body>
 
-        keySong.keyElement.classList.add('ativa')
 
-        if(!keySong.down) {
-            tocaSom(keySong.audioElement)
-        }
-   
-        keySong.down = true
+    <section class="teclado">
+        <button class="tecla tecla_pom">
+            <span class="tecla_title">Rochet</span>
+            <span class="tecla_number"></span>
+        </button>
 
-    }
+        <button class="tecla tecla_clap">
+            <span class="tecla_title">BI</span>
+            <span class="tecla_number"></span>
+        </button>
 
-}
+        <button class="tecla tecla_tim">
+            <span class="tecla_title">Mundial</span>
+            <span class="tecla_number"></span>
+        </button>
 
-document.onkeyup = (event) => {
-    const key = event.key
-    const keySong =  keySongs.find(keySong => keySong.key == key)
-   
-    if(keySong) {
-   
-        keySong.keyElement.classList.remove('ativa')
-        keySong.down = false
 
-    }
+        <button class="tecla tecla_41anoos">
+            <span class="tecla_title">41anoos</span>
+            <span class="tecla_number"></span>
+        </button>
 
-}
+        <button class="tecla tecla_splash">
+            <span class="tecla_title">xojo</span>
+            <span class="tecla_number"></span>
+        </button>
 
+        <button class="tecla tecla_toim">
+            <span class="tecla_title">Rasga</span>
+            <span class="tecla_number"></span>
+        </button>
+
+
+        <button class="tecla tecla_psh">
+            <span class="tecla_title">abelão</span>
+            <span class="tecla_number"></span>
+        </button>
+
+        <button class="tecla tecla_tic">
+            <span class="tecla_title">7 a 1</span>
+            <span class="tecla_number"></span>
+        </button>
+
+        <button class="tecla tecla_tom">
+            <span class="tecla_title">eternF9</span>
+            <span class="tecla_number"></span>
+        </button>
+    </section>
+
+    <audio src="sounds/keya.wav" id="som_tecla_pom"></audio>
+    <audio src="sounds/keyb.wav" id="som_tecla_clap"></audio>
+    <audio src="sounds/keyf.wav" id="som_tecla_tim"></audio>
+    <audio src="sounds/keyi.wav" id="som_tecla_41anoos"></audio>
+    <audio src="sounds/keyg.wav" id="som_tecla_splash"></audio>
+    <audio src="sounds/keyv.wav" id="som_tecla_toim"></audio>
+    <audio src="sounds/keyh.wav" id="som_tecla_psh"></audio>
+    <audio src="sounds/keyç.wav" id="som_tecla_tic"></audio>
+    <audio src="sounds/keyj.wav" id="som_tecla_tom"></audio>
+
+    <script src="main.js"></script>
+</body>
+
+</html>
