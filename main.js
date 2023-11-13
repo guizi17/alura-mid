b0597c42636fc834377f2cabead2fb95449151a2
guizi17 <127101601+guizi17@users.noreply.github.com>
2023-11-13 07:51:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,86 +1,39 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 
-    <title>Internacional audios</title>
+}
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&display=swap" rel="stylesheet">
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-      <link rel="icon" type="image/png" href="imagem/logointer.png">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/estilos.css">
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-</head>
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-<body>
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
+    tecla.onkeydown = function (evento) {
 
-    <section class="teclado">
-        <button class="tecla tecla_pom">
-            <span class="tecla_title">Rochet</span>
-            <span class="tecla_number"></span>
-        </button>
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
 
-        <button class="tecla tecla_clap">
-            <span class="tecla_title">BI</span>
-            <span class="tecla_number"></span>
-        </button>
+    }
 
-        <button class="tecla tecla_tim">
-            <span class="tecla_title">Mundial</span>
-            <span class="tecla_number"></span>
-        </button>
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
-
-        <button class="tecla tecla_41anoos">
-            <span class="tecla_title">41anoos</span>
-            <span class="tecla_number"></span>
-        </button>
-
-        <button class="tecla tecla_splash">
-            <span class="tecla_title">xojo</span>
-            <span class="tecla_number"></span>
-        </button>
-
-        <button class="tecla tecla_toim">
-            <span class="tecla_title">Rasga</span>
-            <span class="tecla_number"></span>
-        </button>
-
-
-        <button class="tecla tecla_psh">
-            <span class="tecla_title">abelão</span>
-            <span class="tecla_number"></span>
-        </button>
-
-        <button class="tecla tecla_tic">
-            <span class="tecla_title">7 a 1</span>
-            <span class="tecla_number"></span>
-        </button>
-
-        <button class="tecla tecla_tom">
-            <span class="tecla_title">eternF9</span>
-            <span class="tecla_number"></span>
-        </button>
-    </section>
-
-    <audio src="sounds/keya.wav" id="som_tecla_pom"></audio>
-    <audio src="sounds/keyb.wav" id="som_tecla_clap"></audio>
-    <audio src="sounds/keyf.wav" id="som_tecla_tim"></audio>
-    <audio src="sounds/keyi.wav" id="som_tecla_41anoos"></audio>
-    <audio src="sounds/keyg.wav" id="som_tecla_splash"></audio>
-    <audio src="sounds/keyv.wav" id="som_tecla_toim"></audio>
-    <audio src="sounds/keyh.wav" id="som_tecla_psh"></audio>
-    <audio src="sounds/keyç.wav" id="som_tecla_tic"></audio>
-    <audio src="sounds/keyj.wav" id="som_tecla_tom"></audio>
-
-    <script src="main.js"></script>
-</body>
-
-</html>
+}
